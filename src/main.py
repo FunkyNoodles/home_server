83c7399525d73c0fac9a13a40998000bc850ed83
FunkyNoodles <nickasds@hotmail.com>
2018-01-17 18:49:38 -0600

    test files

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import http.server
+
+import ssl
+
+server_address = ('localhost', 4443)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='localhost.pem',
+                               ssl_version=ssl.PROTOCOL_SSLv23)
+httpd.serve_forever()
